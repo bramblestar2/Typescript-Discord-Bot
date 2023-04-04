f6353c7dfd59c3bb1b0c5af3e6828e577030a741
Jay Feather2000 <thegals4321@gmail.com>
2023-04-04 12:33:54 -0500

    a

---
+++ TypescriptDiscordBot/deploy-commands.ts
@@ -0,0 +1,56 @@
+import { REST, Routes } from 'discord.js';
+import { clientId, devGuildId, token } from './config.json';
+import fs from 'node:fs';
+import path from 'node:path';
+
+
+const commands = [];
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		commands.push(command.data.toJSON());
+	}
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(token);
+
+
+//// delete for global commands
+//rest.put(Routes.applicationCommands(clientId), { body: [] })
+//	.then(() => console.log('Successfully deleted all application commands.'))
+//	.catch(console.error);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		//// The put method is used to fully refresh all commands in the guild with the current set
+		//const data = await rest.put(
+		//	Routes.applicationGuildCommands(clientId, devGuildId),
+		//	{ body: commands },
+		//);
+
+		//Deploys the commands to all guilds
+		const data : any = await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
